209f476bcaafc3e8d48c29861c4e6a08c493364e
Gabe <36055322+Boundlesss@users.noreply.github.com>
2018-12-01 10:41:29 +0000

    app.js updated!
    I updated the app.js so now you can send multiple requests to multiple webhooks!

--- app.js
+++ app.js
@@ -8,16 +8,6 @@ const c = require('./config.json');
 
 var i = 0;
 
-var options = {
-    url: c.webhook,
-    method: 'POST',
-    json: {
-        "avatar_url": c.avatar_url,
-        "username": c.username,
-        "content": c.content
-    }
-};
-
 function webhook() {
     if (c.loop == true) {
         (function loop() {
@@ -34,14 +24,34 @@ function webhook() {
     }
 }
 
-function send() {
-    request(options, function (err, res, body){
-        if (err) console.log(err);
-    
-        if (!err){
-            console.log(`[POST]: Username: ${c.username}; Content: ${c.content}`);
+function webhookloop() {
+    for (var i = 0; i < c.webhooks.length; i++){
+        console.log(c.webhooks[i]);
+    }
+}
+
+function send() {    
+    var options = {
+        url: c.webhooks[0],
+        method: 'POST',
+        json: {
+            "avatar_url": c.avatar_url,
+            "username": c.username,
+            "content": c.content
         }
-    });
+    };
+
+    for (var i = 0; i < c.webhooks.length; i++){
+        options.url = c.webhooks[i];
+
+        request(options, function (err, res, body){
+            if (err) console.log(err);
+        
+            if (!err){
+                console.log(`[POST]: Username: ${c.username}; Content: ${c.content}`);
+            }
+        });
+    }
 }
 
 webhook();
